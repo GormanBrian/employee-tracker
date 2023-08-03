604e26f76fe5e49ac2be7ffbf238e8874f5ebd16
Brian Gorman <briangorman99@gmail.com>
2023-08-03 12:32:34 -0400

    add newRoleQuestions and root question handling

--- build/src/cli.js
+++ build/src/cli.js
@@ -1,8 +1,67 @@
 import inquirer from 'inquirer';
 const { prompt } = inquirer;
-const questions = [];
-const start = () => prompt(questions).then((answers) => {
-    console.log(answers);
+var RootChoices;
+(function (RootChoices) {
+    RootChoices["ViewAllEmployees"] = "View All Employees";
+    RootChoices["AddEmployee"] = "Add Employee";
+    RootChoices["UpdateEmployeeRole"] = "Update Employee Role";
+    RootChoices["ViewAllRoles"] = "View All Roles";
+    RootChoices["AddRole"] = "Add Role";
+    RootChoices["ViewAllDepartments"] = "View All Departments";
+    RootChoices["AddDepartment"] = "Add Department";
+    RootChoices["Quit"] = "Quit";
+})(RootChoices || (RootChoices = {}));
+const questions = [
+    {
+        message: 'What would you like to do?',
+        name: 'root',
+        type: 'list',
+        choices: Object.values(RootChoices),
+    },
+    {
+        message: 'What is the name of the department?',
+        name: 'newDepartmentName',
+        type: 'input',
+        when: ({ root }) => root === RootChoices.AddDepartment,
+    },
+];
+const newRoleQuestions = [
+    {
+        message: 'What is the name of the role?',
+        name: 'newRoleName',
+        type: 'input',
+    },
+    {
+        message: 'What is the name of the department?',
+        name: 'newDepartmentName',
+        type: 'input',
+    },
+];
+const start = () => prompt(questions).then(async ({ root }) => {
+    switch (root) {
+        case RootChoices.ViewAllEmployees:
+            break;
+        case RootChoices.AddEmployee:
+            break;
+        case RootChoices.UpdateEmployeeRole:
+            break;
+        case RootChoices.ViewAllRoles:
+            break;
+        case RootChoices.AddRole:
+            await prompt(newRoleQuestions).then((answers) => {
+                console.log(answers);
+            });
+            break;
+        case RootChoices.ViewAllDepartments:
+            break;
+        case RootChoices.AddDepartment:
+            break;
+        default:
+            console.error('Invalid choice');
+            break;
+    }
+    if (root !== RootChoices.Quit)
+        start();
 });
 export default start;
 //# sourceMappingURL=cli.js.map
--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,71 @@
-import inquirer from 'inquirer';
-import type { QuestionCollection, Answers } from 'inquirer';
-
+import inquirer, { QuestionCollection, Answers } from 'inquirer';
 const { prompt } = inquirer;
 
-const questions: QuestionCollection = [];
+enum RootChoices {
+  ViewAllEmployees = 'View All Employees',
+  AddEmployee = 'Add Employee',
+  UpdateEmployeeRole = 'Update Employee Role',
+  ViewAllRoles = 'View All Roles',
+  AddRole = 'Add Role',
+  ViewAllDepartments = 'View All Departments',
+  AddDepartment = 'Add Department',
+  Quit = 'Quit',
+}
+
+const questions: QuestionCollection = [
+  {
+    message: 'What would you like to do?',
+    name: 'root',
+    type: 'list',
+    choices: Object.values(RootChoices),
+  },
+  {
+    message: 'What is the name of the department?',
+    name: 'newDepartmentName',
+    type: 'input',
+    when: ({ root }: Answers) => root === RootChoices.AddDepartment,
+  },
+];
+
+const newRoleQuestions: QuestionCollection = [
+  {
+    message: 'What is the name of the role?',
+    name: 'newRoleName',
+    type: 'input',
+  },
+  {
+    message: 'What is the name of the department?',
+    name: 'newDepartmentName',
+    type: 'input',
+  },
+];
 
 const start = () =>
-  prompt(questions).then((answers: Answers) => {
-    console.log(answers);
+  prompt(questions).then(async ({ root }: Answers) => {
+    switch (root) {
+      case RootChoices.ViewAllEmployees:
+        break;
+      case RootChoices.AddEmployee:
+        break;
+      case RootChoices.UpdateEmployeeRole:
+        break;
+      case RootChoices.ViewAllRoles:
+        break;
+      case RootChoices.AddRole:
+        await prompt(newRoleQuestions).then((answers: Answers) => {
+          console.log(answers);
+        });
+        break;
+      case RootChoices.ViewAllDepartments:
+        break;
+      case RootChoices.AddDepartment:
+        break;
+      default:
+        console.error('Invalid choice');
+        break;
+    }
+
+    if (root !== RootChoices.Quit) start();
   });
 
 export default start;
